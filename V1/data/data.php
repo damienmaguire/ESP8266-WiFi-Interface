a700dea9c6afb3ae3a66ce01e06beff8d62bb2a4
Jamie Jones <jamie@mudslidedesign.co.uk>
2020-02-22 15:55:16 +0000

    Wip for selectable display types

--- V1/data/data.php
+++ V1/data/data.php
@@ -1,33 +1,37 @@
 <?php
-$request = $_GET;
-
-if (array_key_exists('p', $request)) {
+$request = $_GET['value'];
+if ('p' == $request) {
   header('Content-Type: text/plain');
   header("HTTP/1.1 200 OK");
   echo rand(0, 100);
-} else if (array_key_exists('v', $request)) {
+} else if ('v' == $request) {
   header('Content-Type: text/plain');
   header("HTTP/1.1 200 OK");
   echo rand(100, 300);
-} else if (array_key_exists('t', $request)) {
+} else if ('t' == $request) {
   header('Content-Type: text/plain');
   header("HTTP/1.1 200 OK");
   echo rand(0, 60);
-} else if (array_key_exists('kwh', $request)) {
+} else if ('kwh' == $request) {
   header('Content-Type: text/plain');
   header("HTTP/1.1 200 OK");
   echo rand(0, 60);
-} else if (array_key_exists('ah', $request)) {
+} else if ('status' == $request) {
   header('Content-Type: text/plain');
   header("HTTP/1.1 200 OK");
   echo rand(0, 60);
-} else if (array_key_exists('chart', $request)) {
+} else if ('ah' ==  $request) {
+  header('Content-Type: text/plain');
+  header("HTTP/1.1 200 OK");
+  echo rand(0, 60);
+
+} else if ('chart' == $request) {
   header('Content-Type: application/javascript');
   header("HTTP/1.1 200 OK");
   $string = file_get_contents(__DIR__."/data.json");
   echo "var chartConfig = " . $string;
   exit();
-} else if (array_key_exists('save', $request)) {
+} else if ('save' == $request) {
 
   //handle post
   $data = $_POST;
@@ -36,9 +40,11 @@ if (array_key_exists('p', $request)) {
 
   $enabled = array_keys($data['chart']);
   foreach($charts as $index=>$chart) {
-    $chart->enabled = in_array($index, $enabled);
-    $chart->min = intval($data['min'][$index]);
-    $chart->max = intval($data['max'][$index]);
+    $chart->type = $data['type'][$index];
+    if ($chart->type == 'gauge') {
+      $chart->min = intval($data['min'][$index]);
+      $chart->max = intval($data['max'][$index]);
+    }
     $chart->updateFrequency = intval($data['frequency'][$index]);
   }
 
